5d57a354251d2a516c3ded8304dd02eca20a8b12
Cristianpardoluna <55992535+CristianCamiloPardoLuna@users.noreply.github.com>
2020-07-29 12:06:21 -0500

    venv creation

--- samples/python/11.qnamaker/config.py
+++ samples/python/11.qnamaker/config.py
@@ -4,15 +4,13 @@
 
 import os
 
-""" Bot Configuration """
-
-
 class DefaultConfig:
-    """ Bot Configuration """
-
+    """ Autenticacion KB de QnATest Indra """
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
+    #7740c0ef-4e61-419a-a8af-aa6ab3647726
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "")
-    QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "")
-    QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "")
+    #wJlL|aDoXKq)OeXT]{6t{iWfOEvgw7y
+    QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "ebc3edff-0672-45b0-a6fd-404b467bf19e")
+    QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "e75add48-dd03-427a-821b-129965bcc03a")
+    QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "https://s0testqnaindra.azurewebsites.net/qnamaker")
